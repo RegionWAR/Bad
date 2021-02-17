5a77eb4739b126e5562c313a6605ba3b3ef48ab3
Mihail GOR <666miha666@bk.ru>
2021-02-17 19:46:15 +0300

    GOOD PASS

---
+++ Good password.py
@@ -0,0 +1,13 @@
+# объявление функции
+def is_password_good(password):
+    txtlist = list(txt)
+    if len(txtlist)>= 8 and txt.lower() != txt and txt.upper() != txt:
+        print("ok")
+    else:
+        print("no")
+
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(is_password_good(txt))
