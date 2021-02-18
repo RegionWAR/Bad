1716b06c4c7e7703ed4df2d94603cddb0649a843
Mihail GOR <666miha666@bk.ru>
2021-02-18 13:21:16 +0300

    OK

--- Good password.py
+++ Good password.py
@@ -1,13 +1,14 @@
 # объявление функции
 def is_password_good(password):
     txtlist = list(txt)
+    passok = False
     if len(txtlist)>= 8 and txt.lower() != txt and txt.upper() != txt:
-        print("ok")
-    else:
-        print("NO")
-
+        for i in txtlist:
+            for m in range(10):
+                if i == str(m):
+                    passok = True
+    return passok
 # считываем данные
 txt = input()
-
 # вызываем функцию
 print(is_password_good(txt))
