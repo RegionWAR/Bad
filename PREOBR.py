81b786c6db1d11d14bb249e49cd4b7a13c3cf472
Mihail GOR <666miha666@bk.ru>
2021-02-22 20:51:23 +0300

    ok

---
+++ PREOBR.py
@@ -0,0 +1,10 @@
+# объявление функции
+def convert_to_python_case(text):
+    result = txt(1)
+    for i in range (1, len(text) + 1)
+
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(convert_to_python_case(txt))
