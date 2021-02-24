c60183da208d71d198138414e0f3bd33719f6d82
Mihail GOR <666miha666@bk.ru>
2021-02-24 16:04:19 +0300

    OK

--- PREOBR.py
+++ PREOBR.py
@@ -1,7 +1,12 @@
 # объявление функции
 def convert_to_python_case(text):
-    result = txt(1)
-    for i in range (1, len(text) + 1)
+    result = txt[0].lower()
+    for i in range (1, len(text)):
+        if txt[i].isupper() == True:
+            result += "_"+ txt[i].lower()
+        else:
+            result += txt[i]
+    return result
 
 # считываем данные
 txt = input()
