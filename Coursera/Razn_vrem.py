313e6421ccac3fd21a85ee25f66b4d68b3b80716
Mihail GOR <666miha666@bk.ru>
2021-04-08 16:09:20 +0300

    OK

--- Coursera/Razn_vrem.py
+++
@@ -1,3 +0,0 @@
-one = (int(input()) * 60 * 60 + int(input()) * 60 + int(input()))
-two = (int(input()) * 60 * 60 + int(input()) * 60 + int(input()))
-print(two - one)
---
+++ Coursera/Week_1/Razn_vrem.py
@@ -0,0 +1,3 @@
+one = int(input()) * 60 * 60 + int(input()) * 60 + int(input())
+two = int(input()) * 60 * 60 + int(input()) * 60 + int(input())
+print(two - one)
