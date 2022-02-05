52509726099dada1bbfbae6dadb1e34c3e703ca2
Mihail GOR <666miha666@bk.ru>
2022-02-05 21:27:20 +0300

    Среднее число

--- JS/Main.java
+++ JS/Main.java
@@ -4,10 +4,10 @@ class MySolution {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int x = sc.nextInt(), y = sc.nextInt(), z = sc.nextInt();
-    if (x > y && x < z) {
+    if ((x >= y && x <= z) || (x <= y && x >= z)) {
       System.out.println(x);
     } else {
-      if (y > x && y < z) {
+      if ((y >= x && y <= z) || (y <= x && y >= z)) {
         System.out.println(y);
       } else {
         System.out.println(z);
